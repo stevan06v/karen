0dc292f5958d9546a2c5d36e011aff0d8bc6ebcf
stevan06v <stevanvlajic5@gmail.com>
2025-01-22 01:57:14 +0100

    new initial commit

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Karen
-This projects uses a micropython websever to chat with an AI-Assistant acting like Karen from Spongebob Squarepants.
+# micropython-web-server-microdot
+## A sample MicroPython Web Server using the MicroDot framework  
+![Featured Image](https://user-images.githubusercontent.com/69466026/204148499-61452724-2021-4170-9686-35310c1278ba.jpg)  
 
-
-![alt text](/imgs/karen.png)
-
+### Writeup
+[How to create a MicroPython Web Server the easy way!](https://www.donskytech.com/how-to-create-a-micropython-web-server-the-easy-way/)
 
--- main.py
+++ main.py
@@ -1,17 +1,37 @@
-from karen.Karen import Karen
-import time
+from microdot_asyncio import Microdot, Response, send_file
+from microdot_utemplate import render_template
+from led_module import LEDModule
+
+app = Microdot()
+Response.default_content_type = 'text/html'
+
+# Our LED Module
+led_module = LEDModule(23)
 
 
-def main():
-    karen = Karen(speaking=True, showing_window=True, displaying_oled=True, logging_messages=True)
-    karen.start()
-
-    karen.print_adapters()
-
-    time.sleep(5)
-
-    karen.stop()
+@app.route('/')
+async def index(request):
+    return render_template('index.html', led_value=led_module.get_value())
 
 
-if __name__ == "__main__":
-    main()
+@app.route('/toggle')
+async def toggle_led(request):
+    print("Receive Toggle Request!")
+    led_module.toggle()
+    return "OK"
+
+
+@app.route('/shutdown')
+async def shutdown(request):
+    request.app.shutdown()
+    return 'The server is shutting down...'
+
+
+@app.route('/static/<path:path>')
+def static(request, path):
+    if '..' in path:
+        # directory traversal is not allowed
+        return 'Not found', 404
+    return send_file('static/' + path)
+
+app.run()
